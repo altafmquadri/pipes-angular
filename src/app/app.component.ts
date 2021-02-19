443ae283591118872baaf4b168fa5c7ebd7e648b
Altaf Quadri <altafmquadri@gmail.com>
2021-02-19 10:52:58 -0500

    using pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.name = value;
+  }
 }
