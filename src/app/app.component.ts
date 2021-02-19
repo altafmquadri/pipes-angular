2a080e20034c5d65787e9fa56df024bbc4ba9a00
Altaf Quadri <altafmquadri@gmail.com>
2021-02-19 15:01:20 -0500

    pipes completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number
+
+  car = {
+    make: 'Honda',
+    model: 'Accord',
+    year: '1995',
+  };
 
   onNameChange(event: Event) {
     const target = event.target as HTMLInputElement;
     const value = target.value;
     this.name = value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.date = value;
+  }
+
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = parseFloat(target.value);
+    this.amount = value;
+  }
+
+  onHeightChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = parseFloat(target.value);
+    this.height = value;
+  }
+
+  onMilesChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = parseFloat(target.value);
+    this.miles = value;
   }
 }
